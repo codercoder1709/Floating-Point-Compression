5c65e82e4deaacdfc6c08a3879945b219da1e06c
Udit Agrawal <uditagrawal1709@gmail.com>
2025-05-30 12:17:20 +0530

    final changes

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -23,41 +23,25 @@ void compressAndVerify(const std::vector<float> &data)
     }
 
     std::cout << (isCorrect ? "Compression/Decompression successful!" : "Compression failed!") << std::endl;
-    std::cout << "Original size: " << data.size() * sizeof(double) << " bytes\n";
-    std::cout << "Compressed size: " << compressed.first.size() << " bytes\n";
-    std::cout << "Compression ratio: "
-              << double(data.size()) * sizeof(double) / compressed.first.size() << "\n\n";
+    std::cout << "Original size: " << data.size() * sizeof(data[0]) << " bytes\n";
+    std::cout << "Compressed size: " << compressed.first.size() * sizeof(data[0]) << " bytes\n";
+    std::cout << "Compression ratio: " << double(data.size()) * sizeof(data[0]) / compressed.first.size() << "\n\n";
 }
-
 
 int main()
 {
     try
     {
-        std::string file_path1 = "../../dataset/largeVolume/city_temperature.csv";
-
-        CSVReader csv1(file_path1);
-        std::vector<std::string> column1 = csv1.extractColumn(7);
-
-        std::vector<float> originalData1 = convertToFloat(column1);
-
-        std::vector<float> data1(originalData1.begin() + 1, originalData1.begin() + 100001);
-
-
-        // Call the modular function
-        compressAndVerify(data1);
         
-        std::string file_path2 = "../../dataset/largeVolume/dataexport_20250126T094958.csv";
+        std::string file_path = "../../dataset/largeVolume/dataexport_20250126T094958.csv";
 
-        CSVReader csv(file_path2);
+        CSVReader csv(file_path);
         std::vector<std::string> column = csv.extractColumnByName("Basel");
         
         std::vector<float> originalData = convertToFloat(column);
 
         std::vector<float> data(originalData.begin() + 1, originalData.begin() + 100001);
-
-
-        // Call the modular function
+        
         compressAndVerify(data);
     }
     catch (const std::exception &ex)
--- dataset/readMe.txt
+++ dataset/readMe.txt
@@ -1,3 +1,5 @@
 pushed to GitHub testDataset used for testing and CI
 
 Not pushed to GitHub largeVolume; used only for internal testing 
+
+You can access the dataset used from "https://drive.google.com/file/d/18LUhuHNixhMxjBzjj8Rvlcqx1rFH3-rx/view?usp=drive_link"
